6c545d4b75dff353a9a8bb46c5359d04dbddd298
muthuselvakumari <muthuselvakumari.m@gmail.com>
2016-09-19 10:39:16 +0530

    Create shutdown.c

---
+++ shutdown.c
@@ -0,0 +1,10 @@
+# guvi
+#include<stdlib.h>
+void main()
+{
+char ch;
+ printf("\n Do you want to shut down the computer or not? (y/n)");
+ scanf("%c",ch);
+ if(ch=='y')
+ system(C:\\WINDOWS\\System32\\Shutdown /s");
+ }
