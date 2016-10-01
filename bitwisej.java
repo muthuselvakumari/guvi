bfccb274902fb2b5508b741390bd678635d2237f
muthuselvakumari <muthuselvakumari.m@gmail.com>
2016-10-01 12:21:19 +0530

    Create bitwisej.java

---
+++ bitwisej.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class bitwisej
+{
+public static void main(String args[])
+{int nn;
+Scanner scn=new Scanner(System.in);
+System.out.println("enter a number");
+nn=scn.nextInt();
+if((nn/2)*2==nn)
+{
+system.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}}}
