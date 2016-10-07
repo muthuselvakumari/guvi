e2a8bbe955b01967815f85dc562b96fead763fbc
muthuselvakumari <muthuselvakumari.m@gmail.com>
2016-10-07 13:50:16 +0530

    Create secondsmall.c

---
+++ secondsmall.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int a[50],size,i,j,small,scnd;
+j=0;
+scanf("%d",&size); //size of array
+scanf("%d",&a[i]); //elements
+small=a[0];
+for(i=1;i<size;i++)
+{
+if(small>a[i])
+{
+small=a[i];
+j=i;
+}}
+scnd=a[size-j-i];
+for(i=1;i<size;i++)
+{
+if(scnd>a[i] && j!=i)
+{
+scnd=a[i];
+}
+printf("\n the second smallest is:%d",scnd);
+}
+}
